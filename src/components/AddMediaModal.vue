9e1711d983970fe1ba063465f991625f8a9782cf
alisondraV <alice.7414122013@gmail.com>
2022-07-20 23:48:46 -0400

    Implement click away for modal

--- src/components/AddMediaModal.vue
+++ src/components/AddMediaModal.vue
@@ -1,29 +1,27 @@
 <template>
-  <div>
-    <div class="fixed inset-0 z-40 grid place-items-center shadow-md">
-      <div class="bg-white p-6 rounded-lg w-1/2">
-        <h1 class="mb-4">Add a new media source</h1>
-        <div class="flex gap-4">
-          <add-media-tile
-            @file-uploaded="$emit('close')"
-            class="flex-1"
-            header="Screenshare"
-            text="Share your entire screen, window or a specific Chrome tab"
-            type="screen"
-          />
-          <add-media-tile
-            @file-uploaded="$emit('close')"
-            class="flex-1"
-            header="Video Feed"
-            text="Share a feed of your in-built webcam and microphone. If you do not have a webcam,
-              you can use a “virtual” webcam such as Streamlabs OBS virtual camera"
-            type="video"
-          />
-        </div>
+  <div class="fixed inset-0 z-40 grid place-items-center shadow-md" @click="$emit('close')">
+    <div class="bg-white p-6 rounded-lg w-1/2" @click.stop="">
+      <h1 class="mb-4">Add a new media source</h1>
+      <div class="flex gap-4">
+        <add-media-tile
+          @file-uploaded="$emit('close')"
+          class="flex-1"
+          header="Screenshare"
+          text="Share your entire screen, window or a specific Chrome tab"
+          type="screen"
+        />
+        <add-media-tile
+          @file-uploaded="$emit('close')"
+          class="flex-1"
+          header="Video Feed"
+          text="Share a feed of your in-built webcam and microphone. If you do not have a webcam,
+            you can use a “virtual” webcam such as Streamlabs OBS virtual camera"
+          type="video"
+        />
       </div>
     </div>
-    <div class="fixed inset-0 z-30 bg-white bg-opacity-70" />
   </div>
+  <div class="fixed inset-0 z-30 bg-white bg-opacity-70" />
 </template>
 
 <script lang="ts" setup>
